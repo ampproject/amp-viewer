55f4d8563a7f6cb97c514a7df9fded495d20f7d2
chenshay <chenshay@google.com>
2017-05-15 10:24:25 -0700

    comments fix

---
+++ src/amp-viewer.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * @fileoverview This file is a Viewer for AMP Documents. 
+ */
+class AmpViewer {
+ 
+ /**
+  * @param {!Element} hostElement the element to attatch the iframe to.
+  * @param {string} ampUrl the AMP page url.
+  */
+ constructor(hostElement, ampUrl) {
+  /** @private {!Element} */
+  this.hostElement_ = hostElement;
+
+  /** @private {string} */
+  this.ampUrl_ = ampUrl;
+
+  /** @private {Element} */
+  this.iframe_ = null;
+ }
+
+ /**
+  * Attaches the AMP Doc Iframe to the Host Element.
+  */
+ attach() {
+   this.iframe_ = document.createElement('iframe');
+   this.hostElement_.appendChild(this.iframe_);
+   this.iframe_.src = this.ampUrl_;
+ }
+
+}
