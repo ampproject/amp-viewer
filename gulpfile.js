8f123fcaed528a3f17aa23bdfba88268efc26cf8
chenshay <chenshay@google.com>
2017-05-22 15:30:09 -0700

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,194 @@
+/**
+ * Copyright 2017 The AMP Start Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const gulp = require('gulp-help')(require('gulp'));
+const util = require('gulp-util');
+const del = require('del');
+const Mustache = require('mustache');
+const posthtml = require('gulp-posthtml');
+const postcss = require('gulp-postcss');
+const runSequence = require('run-sequence');
+const through = require('through2');
+const fs = require('fs');
+const path = require('path');
+const config = require('./tasks/config');
+require('./tasks');
+
+const partialsMap = Object.create(null);
+
+function getData(opt_path) {
+  var path = 'data.json';
+  var jsonPath = (opt_path || '').replace(/html$/, 'json');
+  if (opt_path && jsonPath && fs.existsSync(jsonPath)) {
+    path = jsonPath;
+  }
+  return JSON.parse(fs.readFileSync(path));
+}
+
+function mustacheStream() {
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+    var partials = getPartials(partialsMap,
+        path.dirname(file.path), file.contents.toString());
+    file.contents = new Buffer(Mustache.render(file.contents.toString(),
+        getData(file.path), partials));
+    cb(null, file);
+  });
+}
+
+function getPartials(acc, embedderDir, template) {
+// Assume {{}} as mustache start/end tags
+  const partialRegexp = new RegExp('{{>\\s*(\\S+)\\s*}}', 'g');
+  var partialMatch = null;
+  var partialPath = null;
+  var partialTemplate = null;
+  var absPathToTemplate = null;
+  while ((partialMatch = partialRegexp.exec(template))) {
+    partialPath = partialMatch[1];
+    absPathToTemplate = path.resolve(embedderDir, partialPath);
+    if (!acc[partialPath]) {
+      try {
+        partialTemplate = fs.readFileSync(absPathToTemplate).toString();
+      } catch (e) {
+        util.log(util.colors.red(e.message));
+      }
+      acc[partialPath] = partialTemplate;
+    }
+    getPartials(acc, path.dirname(absPathToTemplate), partialTemplate);
+  }
+  return acc;
+}
+
+gulp.task('build', 'build', function(cb) {
+  runSequence('clean', 'highlight', 'img', 'postcss', 'posthtml', 'www',
+      'validate', 'bundle', cb);
+      // 'validate', cb);
+});
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('img', function() {
+  return gulp.src(config.src.img).pipe(gulp.dest(config.dest.img));
+});
+
+function inlineCheckScript(node) {
+  return false;
+}
+
+function inlineCheckStyle(node) {
+  return node.tag === 'link' && node.attrs && node.attrs.rel === 'stylesheet' &&
+      'amp-custom' in node.attrs && node.attrs.href;
+}
+
+const inlineTransformation = {
+  script: {
+    check: inlineCheckScript,
+  },
+  style: {
+    check: inlineCheckStyle,
+  }
+}
+
+gulp.task('www', function() {
+  const plugins = [
+    require('posthtml-include')({
+      encoding: 'utf-8'
+    }),
+    require('posthtml-inline-assets')({
+      from: config.dest.www_pages,
+      inline: inlineTransformation,
+    }),
+  ];
+  const options = {};
+  return gulp.src(config.src.www_pages)
+    .pipe(mustacheStream())
+    .pipe(posthtml(plugins, options))
+    .pipe(gulp.dest(config.dest.www_pages))
+});
+
+gulp.task('watch', 'watch stuff', ['build'], function() {
+  return gulp.watch([
+    config.src.components,
+    config.src.templates,
+    config.src.www_pages,
+    config.src.css,
+    config.src.data,
+    config.src.img],
+      ['build']);
+});
+
+gulp.task('default', ['build']);
+
+gulp.task('posthtml', 'build kickstart files', function() {
+  const plugins = [
+    require('posthtml-inline-assets')({
+      from: config.dest.templates,
+      inline: inlineTransformation,
+    }),
+    require('posthtml-include')({ encoding: 'utf-8' }),
+  ];
+  const options = {};
+  return gulp.src(config.src.templates)
+    .pipe(mustacheStream())
+    .pipe(posthtml(plugins, options))
+    .pipe(gulp.dest(config.dest.templates))
+});
+
+gulp.task('postcss', 'build postcss files', function() {
+  const plugins = [
+    require('postcss-import')(),
+    require('autoprefixer')(),
+    require('postcss-calc')(),
+    require('postcss-color-function')(),
+    require('postcss-custom-properties')(),
+    require('postcss-discard-comments')(),
+    require('postcss-custom-media')(),
+    require('cssnano')({zindex: false}),
+  ];
+  const replace = require('gulp-replace');
+  const options = {};
+  return gulp.src(config.src.css)
+    .pipe(postcss(plugins, options))
+    .pipe(replace('!important', ''))
+    .pipe(gulp.dest(config.dest.css))
+});
+
+function serve() {
+  var app = require('express')();
+  var webserver = require('gulp-webserver');
+
+  var host = 'localhost';
+  var port = process.env.PORT || 8000;
+  var server = gulp.src(process.cwd())
+      .pipe(webserver({
+        port,
+        host,
+        directoryListing: true,
+        livereload: true,
+        https: false,
+        middleware: [app],
+      }));
+
+  return server;
+}
+
+gulp.task('serve', serve);
+gulp.task('default', serve);
---
+++ tasks/bundle.js
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2017 The AMP Start Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const gulp = require('gulp-help')(require('gulp'));
+const util = require('gulp-util');
+const exec = require('child_process').execSync;
+const jsdom = require('jsdom');
+const through = require('through2');
+const path = require('path');
+const fs = require('fs-extra');
+const config = require('./config');
+const cssbeautify = require('cssbeautify');
+
+function collectResources(filepath, html, done) {
+  const filename = path.basename(filepath, '.amp.html');
+  const env = jsdom.env(html, function(err, window) {
+    const css = window.document.querySelector('style[amp-custom]').textContent;
+    const ampimgs = window.document.querySelectorAll('amp-img[src]');
+    // This can most likely be done with the amp-img scan but separating
+    // out for now.
+    let srcsetimgs = window.document.querySelectorAll('amp-img[srcset]');
+    srcsetimgs = [].concat.apply([], [].slice.call(srcsetimgs)
+        .map(function(ampimg) {
+          return ampimg.getAttribute('srcset')
+              .split(',').map(function(pair) {
+                const src = pair.trim().split(' ')[0];
+                const abspath = path.resolve(path.dirname(filepath), src);
+                return abspath.replace(`${process.cwd()}/`, '');
+              });
+        }));
+    const imgs = [].slice.call(ampimgs).map(function(el) {
+      const src = el.getAttribute('src');
+      const abspath = path.resolve(path.dirname(filepath), src);
+      return abspath.replace(`${process.cwd()}/`, '');
+    });
+    const name = filepath.replace(`${process.cwd()}/`, '');
+    imgs.push.apply(imgs, srcsetimgs);
+    imgs.forEach(function(imgpath) {
+      fs.copySync(imgpath, `.archive/${imgpath.replace(/^dist/, filename)}`);
+    });
+    const pathToTmpl = filepath.replace(/.*templates\/(.*)/, '\$1');
+    fs.copySync(filepath,
+        `.archive/${filename}/templates/${pathToTmpl}`);
+    if (css) {
+      fs.mkdirSync(`.archive/${filename}/css`);
+      fs.writeFileSync(`.archive/${filename}/css/${filename}.max.css`,
+          cssbeautify(css, {indent: '  '}));
+    }
+    fs.writeFileSync(`.archive/${filename}/LICENSE`, licenses);
+    exec(`cd .archive && zip -r ../dist/archive/${filename}.zip ${filename}/`);
+    done();
+  });
+}
+
+
+function bundle() {
+  fs.removeSync('.archive');
+  fs.mkdirSync('.archive');
+
+  fs.removeSync('dist/archive');
+  fs.mkdirSync('dist');
+  fs.mkdirSync('dist/archive');
+
+  return gulp.src(`${config.dest.templates}/templates/**/*.html`)
+      .pipe(through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+          cb(null, file);
+          return;
+        }
+        const resources = collectResources(
+            file.path, file.contents.toString(), cb.bind(null, null, file));
+      })).on('end', function() {
+        fs.removeSync('.archive');
+      });
+}
+
+gulp.task('bundle', bundle);
+
+const licenses = `
+Basscss | https://github.com/basscss/basscss/blob/master/LICENSE.md
+----------------------------------------------------------------------------
+
+# The MIT License (MIT)
+
+Copyright © Nicolas Gallagher and Jonathan Neal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Normalize.css | https://github.com/necolas/normalize.css/blob/master/LICENSE.md
+----------------------------------------------------------------------------
+# The MIT License (MIT)
+Copyright (c) 2013 – 2016 Brent Jackson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+AMP Start | https://github.com/ampproject/ampstart/blob/master/LICENSE
+----------------------------------------------------------------------------
+Copyright 2017 The AMP Start Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`;
